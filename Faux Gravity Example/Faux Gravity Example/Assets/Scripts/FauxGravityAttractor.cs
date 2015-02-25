66c87b8e7ef16600bf21bddc530af8342535983b
codysblanchard <codysblanchard@gmail.com>
2015-02-24 16:59:35 -0800

    intial commit

---
+++ Faux Gravity Example/Faux Gravity Example/Assets/Scripts/FauxGravityAttractor.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FauxGravityAttractor : MonoBehaviour {
+
+	public float gravity = -12;
+
+	public float jumpPower = 200;
+
+
+	public void Attract(Transform body) {
+		Vector3 gravityUp = (body.position - transform.position).normalized;
+
+
+		Vector3 localUp = body.up;
+
+		body.rigidbody.AddForce(gravityUp * gravity);
+
+		Quaternion targetRotation = Quaternion.FromToRotation(localUp,gravityUp) * body.rotation;
+		 body.rotation = Quaternion.Slerp(body.rotation,targetRotation,50f * Time.deltaTime );
+
+
+		if (Input.GetMouseButtonUp (0) || Input.GetButtonDown ("Fire1")) {
+
+			body.rigidbody.AddForce(gravityUp *  jumpPower);
+
+				}
+
+
+	}   
+
+}
+
+//
---
+++ Faux Gravity Example/Faux Gravity Example/Assets/Scripts/rotate.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotate : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		if (homePlanet) {
+			speed = homePlanetSpeed;
+		}
+		else speed = Random.Range (2,6);
+	}
+	private float speed = 10F;
+	public bool homePlanet=false;
+	public float homePlanetSpeed = 2F;
+	
+	void Update ()
+	{
+		transform.Rotate(Vector3.up, speed * Time.deltaTime);
+	}
+}
